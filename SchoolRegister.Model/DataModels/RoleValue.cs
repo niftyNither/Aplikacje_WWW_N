ad170832f2c676a13277b03a6fb594f7e373c4f9
nifty <m00nl16h7t@gmail.com>
2022-04-01 23:23:25 +0200

    an update

---
+++ SchoolRegister.Model/DataModels/RoleValue.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolRegister.Model.DataModels
+{
+    public enum RoleValue
+    {
+        User = 1,
+        Student = 1,
+        Parent = 1,
+        Teacher = 1,
+        Admin = 1,
+    }
+}
